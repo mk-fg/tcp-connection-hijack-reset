3cd9054560bcf28a2d2f1f3f867d7e7410be856d
Mike Kazantsev <mk.fraggod@gmail.com>
2013-04-07 19:58:46 +0600

    Failed attempt to bait some traffic with invalid packets

--- tcp-connection-hijack-reset.py
+++ tcp-connection-hijack-reset.py
@@ -10,29 +10,51 @@ from scapy.all import *
 
 class TCPBreaker(Automaton):
 
-	ss_filter_port = ss_remote = None
-	ss_intercept = list()
+	ss_filter_port_local = ss_filter_port_remote = None
+	ss_remote, ss_intercept = None, list()
 
-	def __init__(self, port, **atmt_kwz):
-		self.ss_filter_port = port
+	def __init__(self, port_local=None, port_remote=None, **atmt_kwz):
+		self.ss_filter_port_local, self.ss_filter_port_remote = port_local, port_remote
 		super(TCPBreaker, self).__init__(**atmt_kwz)
 
 
 	def pkt_filter(self, pkt):
+		'Filter packets by additional (to bpf) criterias, passed on init.'
 		if IP not in pkt or TCP not in pkt: raise KeyError
-		elif pkt[TCP].dport == self.ss_filter_port: return pkt[IP].dst
-		elif pkt[TCP].sport == self.ss_filter_port: return pkt[IP].src
-		else: raise KeyError
+		if pkt.seq == 0: raise KeyError # ack-bait packet
+		for port, res_dport, res_sport in [
+				(self.ss_filter_port_local, 'src', 'dst'),
+				(self.ss_filter_port_remote, 'dst', 'src') ]:
+			if port is None: continue
+			for chk, res in it.izip(['dport', 'sport'], [res_dport, res_sport]):
+				if getattr(pkt[TCP], chk) == port: return getattr(pkt[IP], res)
+		raise KeyError
+
+	def get_remote_by_port(self):
+		'Lookup remote host for a specified port(s) in tcp connection table.'
+		with open('/proc/net/tcp') as src:
+			src = iter(src)
+			next(src) # skip header line
+			for line in src:
+				local, remote = (
+					tuple(int(v, 16) for v in ep.split(':'))
+					for ep in op.itemgetter(1, 2)(line.split()) )
+				for (ep, port) in [
+						(local, self.ss_filter_port_local),
+						(remote, self.ss_filter_port_remote) ]:
+					if ep[1] == port: break
+				else: continue
+				return inet_ntoa(struct.pack(b'<I', remote[0] & 0xffffffff)), local[1], remote[1]
+			else: raise KeyError
 
 
 	@ATMT.state(initial=1)
 	def st_seek(self):
 		log.debug('Waiting for noise on the session')
-		pass
 
 	@ATMT.receive_condition(st_seek)
 	def check_packet(self, pkt):
-		log.debug('Session check, packet: {}'.format(pkt.summary()))
+		log.debug('Session check, packet: {!r}'.format(pkt))
 		try: remote = self.pkt_filter(pkt)
 		except KeyError: return
 		raise self.st_collect(pkt).action_parameters(remote)
@@ -42,12 +64,17 @@ class TCPBreaker(Automaton):
 			log.debug('Found session (remote: {})'.format(remote))
 			self.ss_remote = remote
 
-	@ATMT.timeout(st_seek, 3)
+	@ATMT.timeout(st_seek, 1)
 	def interception_done(self):
 		if self.ss_remote and self.ss_intercept:
-			log.debug('Session data collected, proceeding to termination')
-			raise self.st_fin_send()
-		log.debug('Waiting for session data')
+			log.debug('Captured seq, proceeding to termination')
+			raise self.st_rst_send()
+		else:
+			remote, sport, dport = self.get_remote_by_port()
+			log.debug('Provoking remote ({}) to send correcting ACK'.format(remote))
+			# Contrary to what digitage.co.uk/digitage/software/cutter says, it doesn't work ;(
+			pkt = IP(dst=remote)/TCP(sport=sport, dport=dport, seq=0, flags=b'PA')#/b'XXX'
+			send(pkt)
 
 	@ATMT.state()
 	def st_collect(self, pkt):
@@ -56,19 +83,18 @@ class TCPBreaker(Automaton):
 		raise self.st_seek()
 
 	@ATMT.state()
-	def st_fin_send(self):
-		log.debug('Sending FIN packet')
+	def st_rst_send(self):
 		pkt_eth, pkt_ip, pkt_tcp = op.itemgetter(Ether, IP, TCP)(self.ss_intercept[-1])
-		ordered = lambda k1,v1,k2,v2,pkt_dir=(pkt_ip.dst != self.ss_remote):\
+		ordered = lambda k1,v1,k2,v2,pkt_dir=(pkt_ip.dst == self.ss_remote):\
 			dict(it.izip((k1,k2), (v1,v2) if pkt_dir else (v2,v1)))
-		rst = Ether(**ordered('src', pkt_eth.dst, 'dst', pkt_eth.src))\
+		rst = Ether(**ordered('dst', pkt_eth.dst, 'src', pkt_eth.src))\
 			/ IP(**ordered('src', pkt_ip.src, 'dst', pkt_ip.dst))\
 			/ TCP(**dict(it.chain.from_iterable(p.viewitems() for p in (
 				ordered('sport', pkt_tcp.sport, 'dport', pkt_tcp.dport),
 				ordered('seq', pkt_tcp.seq, 'ack', pkt_tcp.ack),
 				dict(flags=b'FA', window=pkt_tcp.window) ))))
 		rst[TCP].ack += len(pkt_tcp.payload)
-		log.debug('RST: {}'.format(rst.summary()))
+		log.debug('Sending RST: {!r}'.format(rst))
 		sendp(rst)
 		self.stop()
 
@@ -80,7 +106,10 @@ def main(argv=None):
 			' Uses Linux tcp_diag interface or captured traffic to get connection sequence'
 				' number and inject RST packet into it, killing it regardless of how active it is'
 				' and without resorting to bruteforce-guessing of seq numbers.')
-	parser.add_argument('port', type=int, help='TCP port of connection to close.')
+	parser.add_argument('port', type=int,
+		help='TCP port of local (unless --remote-port is specified) connection to close.')
+	parser.add_argument('-d', '--remote-port', action='store_true',
+		help='Treat port argument as remote, not local one.')
 	parser.add_argument('-f', '--bpf', metavar='bpf_filter_string',
 		help='BPF (Berkley Packet Filter) format string to'
 				' passively snatch packets on relevant connection with.'
@@ -97,7 +126,9 @@ def main(argv=None):
 	log = logging.getLogger()
 
 	if not optz.bpf: optz.bpf = 'port {}'.format(optz.port)
-	TCPBreaker(port=optz.port, store=False, filter='tcp and {}'.format(optz.bpf)).run()
+	TCPBreaker(
+		store=False, filter='tcp and {}'.format(optz.bpf),
+		**{('port_local' if not optz.remote_port else 'port_remote'): optz.port} ).run()
 
 
 if __name__ == '__main__': sys.exit(main())
