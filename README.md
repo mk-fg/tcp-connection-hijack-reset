ecc260db60bbdb92c0842d5b02f97cc3c597adef
Mike Kazantsev <mk.fraggod@gmail.com>
2013-04-11 02:15:21 +0600

    Updated to work only with iptables/ipsets and nflog

--- README.md
+++ README.md
@@ -1,84 +1,20 @@
 tcp-connection-hijack-reset
 --------------------
 
-Simple [scapy](http://www.secdev.org/projects/scapy/)-based tool to hijack and
-reset existing TCP connections, established from other pids.
+Simple [scapy](http://www.secdev.org/projects/scapy/) + iptables/ipsets + nflog
+tool to hijack and reset existing TCP connections (for both ends), established
+from other pids.
 
 Purpose is not some malicious DoS attacks but rather kicking hung state-machines
 in otherwise nice software, while making the whole thing look like a random net
 hiccup, which most apps are designed to handle.
 
-Tool doesn't require any firewall configuration changes, but it flips network
-interfaces into promiscuous mode for the time it runs.
-
 
 Usage
 --------------------
 
-Imagine two hosts happily chatting over netcat:
+To be updated.
 
-	host1% ncat -v -l 0.0.0.0 1234
-	Ncat: Listening on 0.0.0.0:1234.
-
-	host2% ncat -v host1 1234
-	Ncat: Connected to host1:1234.
-
-Yet root on host2 doesn't want them to chat...
-
-	host2# ./tcp-connection-hijack-reset.py --debug --remote-port 1234
-	DEBUG:root:Waiting for noise on the session
-	DEBUG:root:Session check, packet: <...>
-	DEBUG:root:Found session (remote: host1)
-	DEBUG:root:Collected: Ether / IP / TCP ...
-	DEBUG:root:Captured seq, proceeding to termination
-	DEBUG:root:Sending FIN: <...>
-
-That should promptly terminate the connection.
-
-Note that lacking implementation of some of the things described in an
-"enhancements" section below, tool requires at least some traffic to pass on
-connection, which is usually not the case with various hangs which need it the
-most.
-
-One simple soluton here is SO_KEEPALIVE socket flag, which can be enabled
-system-wide even on linux with something like
-[libkeepalive](http://libkeepalive.sourceforge.net/) in ld.so.preload and
-further configured to send empty keepalive packets at shorter intervals than the
-default 2h or so.
-
-As any such packet has seq in it, and is enough for the thing to kill the
-connection.
-
-
-Possible enhancements
---------------------
-
-- Will need to check if scapy supports capturing packets via
-	[nflog](http://wiki.wireshark.org/CaptureSetup/NFLOG) - probably does already,
-	or should be trivial to add, as underlying tcpdump definitely has support for
-	it - and add an option for using it to acquire packets.
-
-	Should give some performance boost and make the thing a bit more flexible at
-	the expense of some required system configuration.
-
-- Implement (or rather just copy from [cpiu](http://criu.org/),
-	[ptrace-parasite](https://code.google.com/p/ptrace-parasite/) or similar
-	project) a way to hijack pids and either do `getsockopt(sk, SOL_TCP,
-	TCP_QUEUE_SEQ, ...)` in them or just close right there connections.
-
-	At the moment, crtools still seem to require kernel patches and the voodoo
-	approaches to shellcode generation and injection employed in these projects
-	seem to need a bit of hacking to reuse - no nicely packaged lib available yet.
-
-- Find some other (instant) way to spoof tcp seq numbers from kernel - ideally,
-	some root-only /proc hook would just dump these - why the hell not?
-
-	Wasn't able to find any, but didn't look hard enough to various netlink
-	options available, so maybe there is a way already.
-
-- More convenient CLI and operation - allow to specify pid to hijack connections
-	from, stick around to check if fin was sent from remote, configurable
-	hijack-delay, option to set a timeout for operation.
 
 
 Similar tools
--- tcp-connection-hijack-reset.py
+++ tcp-connection-hijack-reset.py
@@ -1,50 +1,93 @@
 #!/usr/bin/env python
-from __future__ import unicode_literals, print_function
-
-import itertools as it, operator as op, functools as ft
-import os, sys, signal
+from __future__ import print_function
 
 from scapy.all import *
+
+import itertools as it, operator as op, functools as ft
+from subprocess import Popen, PIPE
+from os.path import join
+import os, sys, signal, re
+
+from scapy_nflog import NFLOGListenSocket
+
+
+class ConnMultipleMatches(Exception): pass
+class ConnNotFound(Exception): pass
+
+
+def get_endpoints(pid=None, port_local=None, port_remote=None):
+	if pid: matches = list(get_endpoints_by_pid(pid))
+	else:
+		matches = list(get_endpoints_by_port(
+			port_local=None, port_remote=None ))
+
+	if not matches: raise ConnNotFound()
+	if len(matches) > 1: raise ConnMultipleMatches(matches)
+
+	local, remote = matches[0]
+	return (inet_ntoa(struct.pack('<I', local[0] & 0xffffffff)), local[1]),\
+		(inet_ntoa(struct.pack('<I', remote[0] & 0xffffffff)), remote[1])
+
+def get_endpoints_by_port(port_local=None, port_remote=None):
+	'Lookup connection for a specified port(s) in /proc/net/tcp table.'
+	assert port_local or port_remote, (port_local, port_remote)
+	with open('/proc/net/tcp') as src:
+		src = iter(src)
+		next(src) # skip header line
+		for line in src:
+			local, remote = (
+				tuple(int(v, 16) for v in ep.split(':'))
+				for ep in op.itemgetter(1, 2)(line.split()) )
+			for ep, port in [(local, port_local), (remote, port_remote)]:
+				if ep[1] == port: break
+			else: continue
+			yield local, remote
+
+def get_endpoints_by_pid(pid):
+	'Lookup connection(s) for a specified pid.'
+	assert isinstance(pid, int), pid
+	conns = dict()
+	with open('/proc/net/tcp') as src:
+		src = iter(src)
+		next(src) # skip header line
+		for line in src:
+			line = line.split()
+			conns[line[9]] = line[1], line[2]
+	pid_fds = '/proc/{}/fd'.format(pid)
+	for fd in os.listdir(pid_fds):
+		match = re.search(r'^socket:\[(\d+)\]$', os.readlink(join(pid_fds, fd)))
+		if not match: continue
+		try: match = conns[match.group(1)]
+		except KeyError: continue
+		yield tuple(tuple(int(v, 16) for v in ep.split(':')) for ep in match)
+
+
+def ipset_update(cmd, name, ip, port):
+	if name is None: return
+	assert cmd in ['add', 'del'], cmd
+	log.debug('Updating ipset {!r} ({} {}:{})'.format(name, cmd, ip, port))
+	if Popen(['ipset', cmd, name, '{},{}'.format(ip, port)]).wait():
+		raise RuntimeError('Failed running ipset command, see error output above.')
+
 
 
 class TCPBreaker(Automaton):
 
 	ss_filter_port_local = ss_filter_port_remote = None
-	ss_remote, ss_intercept = None, list()
+	ss_remote, ss_intercept = None, None
 
-	def __init__(self, port_local=None, port_remote=None, **atmt_kwz):
+	def __init__(self, port_local, port_remote, **atmt_kwz):
 		self.ss_filter_port_local, self.ss_filter_port_remote = port_local, port_remote
+		atmt_kwz.setdefault('store', False)
+		atmt_kwz.setdefault( 'filter',
+			'tcp and src port {} and dst port {}'.format(port_local, port_remote) )
 		super(TCPBreaker, self).__init__(**atmt_kwz)
 
-
 	def pkt_filter(self, pkt):
-		'Filter packets by additional (to bpf) criterias, passed on init.'
-		if IP not in pkt or TCP not in pkt: raise KeyError
-		for port, res_dport, res_sport in [
-				(self.ss_filter_port_local, 'src', 'dst'),
-				(self.ss_filter_port_remote, 'dst', 'src') ]:
-			if port is None: continue
-			for chk, res in it.izip(['dport', 'sport'], [res_dport, res_sport]):
-				if getattr(pkt[TCP], chk) == port: return getattr(pkt[IP], res)
+		'Filter packets by ports in case of capture filter leaks or dissector bugs.'
+		if {pkt[TCP].sport, pkt[TCP].dport} == {self.ss_filter_port_local, self.ss_filter_port_remote}:
+			return pkt[IP].dst if pkt[TCP].sport == self.ss_filter_port_local else pkt[IP].src
 		raise KeyError
-
-	def get_remote_by_port(self):
-		'Lookup remote host for a specified port(s) in tcp connection table.'
-		with open('/proc/net/tcp') as src:
-			src = iter(src)
-			next(src) # skip header line
-			for line in src:
-				local, remote = (
-					tuple(int(v, 16) for v in ep.split(':'))
-					for ep in op.itemgetter(1, 2)(line.split()) )
-				for (ep, port) in [
-						(local, self.ss_filter_port_local),
-						(remote, self.ss_filter_port_remote) ]:
-					if ep[1] == port: break
-				else: continue
-				return inet_ntoa(struct.pack(b'<I', remote[0] & 0xffffffff)), local[1], remote[1]
-			else: raise KeyError
-
 
 	@ATMT.state(initial=1)
 	def st_seek(self):
@@ -62,54 +105,54 @@ class TCPBreaker(Automaton):
 			log.debug('Found session (remote: {})'.format(remote))
 			self.ss_remote = remote
 
-	@ATMT.timeout(st_seek, 1)
-	def interception_done(self):
-		if self.ss_remote and self.ss_intercept:
-			log.debug('Captured seq, proceeding to termination')
-			raise self.st_fin_send()
-
 	@ATMT.state()
 	def st_collect(self, pkt):
 		log.debug('Collected: {}'.format(pkt.summary()))
-		self.ss_intercept.append(pkt)
-		raise self.st_seek()
+		self.ss_intercept = pkt
+		raise self.st_rst_send()
 
 	@ATMT.state()
-	def st_fin_send(self):
-		pkt_eth, pkt_ip, pkt_tcp = op.itemgetter(Ether, IP, TCP)(self.ss_intercept[-1])
-		ordered = lambda k1,v1,k2,v2,pkt_dir=(pkt_ip.dst == self.ss_remote):\
-			dict(it.izip((k1,k2), (v1,v2) if pkt_dir else (v2,v1)))
-		fin = Ether(**ordered('dst', pkt_eth.dst, 'src', pkt_eth.src))\
-			/ IP(**ordered('src', pkt_ip.src, 'dst', pkt_ip.dst))\
-			/ TCP(**dict(it.chain.from_iterable(p.viewitems() for p in (
-				ordered('sport', pkt_tcp.sport, 'dport', pkt_tcp.dport),
-				ordered('seq', pkt_tcp.seq, 'ack', pkt_tcp.ack),
-				dict(flags=b'FA', window=pkt_tcp.window) ))))
-		fin[TCP].ack += len(pkt_tcp.payload)
-		log.debug('Sending FIN: {!r}'.format(fin))
-		sendp(fin)
+	def st_rst_send(self):
+		pkt_ip, pkt_tcp = op.itemgetter(IP, TCP)(self.ss_intercept)
+		rst = IP(src=pkt_ip.src, dst=pkt_ip.dst)\
+			/ TCP( flags='R', window=pkt_tcp.window,
+				sport=pkt_tcp.sport, dport=pkt_tcp.dport,
+				seq=pkt_tcp.seq, ack=pkt_tcp.ack )
+		log.debug('Sending RST: {!r}'.format(rst))
+		send(rst)
+
+		# Finish
 		os.kill(os.getpid(), signal.SIGINT)
 		self.stop()
+
 
 
 def main(argv=None):
 	import argparse
 	parser = argparse.ArgumentParser(
 		description='TCP connection breaking tool.'
-			' Uses Linux tcp_diag interface or captured traffic to get connection sequence'
-				' number and inject FIN packet into it, killing it regardless of how active it is'
-				' and without resorting to bruteforce-guessing of seq numbers.')
-	parser.add_argument('port', type=int,
+			' Finds connection with a specified parameters and adds local'
+				' ip:port to the specified ipset, so firewall can cut it on first activity'
+				' (with something like "-j REJECT --reject-with tcp-reset").'
+			' Also uses captured traffic to get connection sequence number and send'
+				' RST packet to remote endpoint, removing ip:port from ipset upon success.')
+
+	parser.add_argument('ipset_name',
+		help='Name of the ipset to temporarily insert local ip:port to.'
+			' Should be used in some iptables filter rule,'
+				' rejecting outgoing packets from it with tcp-reset packet.'
+			' If not specified, no ipset updates will be performed.')
+
+	parser.add_argument('--pid', type=int,
+		help='Process ID to terminate connection of.')
+	# parser.add_argument('-i', '--interactive', action='store_true',
+	# 	help='With --pid option, allow to pick connection in CLI, if there is more then one.')
+
+	parser.add_argument('--port', type=int,
 		help='TCP port of local (unless --remote-port is specified) connection to close.')
 	parser.add_argument('-d', '--remote-port', action='store_true',
 		help='Treat port argument as remote, not local one.')
-	parser.add_argument('-f', '--bpf', metavar='bpf_filter_string',
-		help='BPF (Berkley Packet Filter) format string to'
-				' passively snatch packets on relevant connection with.'
-			' Prepended with "tcp and" for convenience,'
-				' so that "port 5190" argument will result in "tcp and port 5190" filter.'
-			' If not specified, auto-generated from provided IPs'
-				' and/or port numbers (e.g. "port 5190" from "5190" as port argument).')
+
 	parser.add_argument('--debug', action='store_true', help='Verbose operation mode.')
 	optz = parser.parse_args(sys.argv[1:] if argv is None else argv)
 
@@ -118,15 +161,25 @@ def main(argv=None):
 	global log
 	log = logging.getLogger()
 
+	## Configure scapy
 	# It should be traffic to/from same machine - no promisc-mode necessary
 	conf.promisc = conf.sniff_promisc = False
 	# Disables "Sent 1 packets." line
 	conf.verb = False
+	# Install NFLOG listener
+	conf.L2listen = NFLOGListenSocket
 
-	if not optz.bpf: optz.bpf = 'port {}'.format(optz.port)
-	TCPBreaker(
-		store=False, filter='tcp and {}'.format(optz.bpf),
-		**{('port_local' if not optz.remote_port else 'port_remote'): optz.port} ).run()
+	if optz.port:
+		local, remote = get_endpoints(
+			**{'port_local' if not optz.remote_port else 'port_remote': optz.port} )
+	elif optz.pid: local, remote = get_endpoints(pid=optz.pid)
+	else: parser.error('No connection-picking criterias specified.')
+
+	log.debug('Found connection: {0[0]}:{0[1]} -> {1[0]}:{1[1]}'.format(local, remote))
+
+	ipset_update('add', optz.ipset_name, *local)
+	try: TCPBreaker(port_local=local[1], port_remote=remote[1]).run()
+	finally: ipset_update('del', optz.ipset_name, *local)
 
 
 if __name__ == '__main__': sys.exit(main())
